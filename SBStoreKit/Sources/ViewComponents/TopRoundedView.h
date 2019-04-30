5153f6cfcb5b4b1decdf9f2b24fee34ec61b66eb
Mehdi Hosseinzadeh <mehdi.hosseinzadeh86@gmail.com>
2019-04-30 13:22:11 +0430

    Improved phone getting screen

---
+++ SBStoreKit/Sources/ViewComponents/TopRoundedView.h
@@ -0,0 +1,17 @@
+//
+//  TopRoundedView.h
+//  SBStoreKit
+//
+//  Created by Mehdi on 4/30/19.
+//  Copyright © 2019 Sibche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TopRoundedView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SBStoreKit/Sources/ViewComponents/TopRoundedView.m
@@ -0,0 +1,31 @@
+//
+//  TopRoundedView.m
+//  SBStoreKit
+//
+//  Created by Mehdi on 4/30/19.
+//  Copyright © 2019 Sibche. All rights reserved.
+//
+
+#import "TopRoundedView.h"
+
+@implementation TopRoundedView
+
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    [self roundCorners];
+}
+
+- (void)roundCorners {
+    UIBezierPath *maskPath = [UIBezierPath
+                              bezierPathWithRoundedRect:self.bounds
+                              byRoundingCorners:(UIRectCornerTopLeft | UIRectCornerTopRight)
+                              cornerRadii:CGSizeMake(20, 20)
+                              ];
+    CAShapeLayer *maskLayer = [CAShapeLayer layer];
+    
+    maskLayer.frame = self.bounds;
+    maskLayer.path = maskPath.CGPath;
+    self.layer.mask = maskLayer;
+}
+
+@end
