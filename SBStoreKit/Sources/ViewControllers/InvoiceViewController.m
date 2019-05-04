32fa34157ff37679fa37be0d42da29340916391b
Mehdi Hosseinzadeh <mehdi.hosseinzadeh86@gmail.com>
2019-05-04 16:49:14 +0430

    Removed add credit view. Now everything handled in invoice view

--- SBStoreKit/Sources/ViewControllers/InvoiceViewController.m
+++ SBStoreKit/Sources/ViewControllers/InvoiceViewController.m
@@ -18,12 +18,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *loadingLabel;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loadingIndicator;
 @property (weak, nonatomic) IBOutlet UIView *loadingView;
-@property (weak, nonatomic) IBOutlet UIView *invoiceView;
 @property (weak, nonatomic) IBOutlet FilledButton *confirmButton;
 
 
 @property (weak, nonatomic) IBOutlet UILabel *packageNameLabel;
-@property (weak, nonatomic) IBOutlet UILabel *packageDescriptionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *packagePriceLabel;
 @property (weak, nonatomic) IBOutlet UILabel *userBalanceLabel;
 
@@ -31,26 +29,59 @@
 
 @implementation InvoiceViewController
 
--(void)viewWillAppear:(BOOL)animated{
+- (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [self loadPurchasablePackage];
+
+    [[NSNotificationCenter defaultCenter] addObserverForName:nil object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+        [self newNotification:note];
+    }];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (IBAction)closeButtonPressed:(id)sender {
-    [self paymentCanceled];
+    [self paymentCanceledWithNotifying:NO];
 }
 
-- (void)paymentSucceeded{
-    [[NSNotificationCenter defaultCenter] postNotificationName:PAYMENT_SUCCESSFUL object:nil];
-}
-
-- (void)paymentCanceled{
+- (void)paymentSucceeded {
     dispatch_async(dispatch_get_main_queue(), ^{
-        [[NSNotificationCenter defaultCenter] postNotificationName:PAYMENT_CANCELED object:nil];
-        [self dismissViewControllerAnimated:YES completion:nil];
+        [self.confirmButton removeTarget:self action:@selector(sendToAddCreditAndPurchase:) forControlEvents:UIControlEventTouchUpInside];
+        [self.confirmButton removeTarget:self action:@selector(confirmPurchase:) forControlEvents:UIControlEventTouchUpInside];
+        
+        self.confirmButton.backgroundColor = [UIColor colorWithRed:0 green:200.0/255.0 blue:83.0/255.0 alpha:1];
+        [self.confirmButton setTitle:@"پرداخت شما با موفقیت انجام شد" forState:UIControlStateNormal];
+        
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [[NSNotificationCenter defaultCenter] postNotificationName:PAYMENT_SUCCESSFUL object:nil];
+            [self dismissViewControllerAnimated:YES completion:nil];
+        });
     });
 }
 
+- (void)paymentCanceledWithNotifying:(BOOL)withNotifying {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (withNotifying) {
+            [self.confirmButton removeTarget:self action:@selector(sendToAddCreditAndPurchase:) forControlEvents:UIControlEventTouchUpInside];
+            [self.confirmButton removeTarget:self action:@selector(confirmPurchase:) forControlEvents:UIControlEventTouchUpInside];
+            
+            self.confirmButton.backgroundColor = UIColor.redColor;
+            [self.confirmButton setTitle:@"عدم پرداخت موفق" forState:UIControlStateNormal];
+            
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC));
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                [[NSNotificationCenter defaultCenter] postNotificationName:PAYMENT_CANCELED object:nil];
+                [self dismissViewControllerAnimated:YES completion:nil];
+            });
+        }else{
+            [[NSNotificationCenter defaultCenter] postNotificationName:PAYMENT_CANCELED object:nil];
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }
+    });
+}
 
 - (void)loadPurchasablePackage{
     [self setLoading:YES withMessage:@""];
@@ -60,9 +91,10 @@
     [[NetworkManager sharedManager] post:url withData:nil withAdditionalHeaders:nil withToken:token withSuccess:^(NSString *response, NSDictionary *json) {
         [DataManager sharedManager].showingInvoiceData = json;
         [self fillPackageData];
-        [self setLoading:NO withMessage:@""];
+//        [self setLoading:NO withMessage:@""];
     } withFailure:^(NSInteger errorCode, NSInteger httpStatusCode) {
-        [self setLoading:NO withMessage:@"در روند خرید مشکلی پیش آمد. لطفا دوباره امتحان کنید."];
+        // TODO: We should handle errors in a better way
+//        [self setLoading:NO withMessage:@"در روند خرید مشکلی پیش آمد. لطفا دوباره امتحان کنید."];
     }];
 }
 
@@ -71,11 +103,9 @@
         NSDictionary* showingInvoiceData = [DataManager sharedManager].showingInvoiceData;
         if (showingInvoiceData) {
             NSString* name = [showingInvoiceData valueForKeyPath:@"data.attributes.name"];
-            NSString* description = [showingInvoiceData valueForKeyPath:@"data.attributes.description"];
             NSNumber* totalPrice = [showingInvoiceData valueForKeyPath:@"data.attributes.total_price"];
             NSNumber* price = [showingInvoiceData valueForKeyPath:@"data.attributes.price"];
             self.packageNameLabel.text = name;
-            self.packageDescriptionLabel.text = description;
             NSMutableAttributedString* attributedText = nil;
             NSString* priceText = [NSString stringWithFormat:@"%@ تومان", [SibcheHelper formatNumber:price]];
             NSString* totalPriceText = [NSString stringWithFormat:@"%@ تومان", [SibcheHelper formatNumber:totalPrice]];
@@ -102,16 +132,15 @@
             NSString* balanceText = @"";
             NSString* buttonTitle = @"";
             
+            balanceText = [NSString stringWithFormat:@"موجودی: %@ تومان", [SibcheHelper formatNumber:[NSNumber numberWithInt:balance]]];
+            buttonTitle = @"پرداخت";
+
             if (balance >= [totalPrice intValue]) {
-                balanceText = [NSString stringWithFormat:@"موجودی: %@ تومان", [SibcheHelper formatNumber:[NSNumber numberWithInt:balance]]];
-                buttonTitle = @"پرداخت";
-                [self.confirmButton removeTarget:self action:@selector(addCredit:) forControlEvents:UIControlEventTouchUpInside];
+                [self.confirmButton removeTarget:self action:@selector(sendToAddCreditAndPurchase:) forControlEvents:UIControlEventTouchUpInside];
                 [self.confirmButton addTarget:self action:@selector(confirmPurchase:) forControlEvents:UIControlEventTouchUpInside];
             }else{
-                balanceText = @"موجودی شما کافی نیست.";
-                buttonTitle = @"افزایش اعتبار";
                 [self.confirmButton removeTarget:self action:@selector(confirmPurchase:) forControlEvents:UIControlEventTouchUpInside];
-                [self.confirmButton addTarget:self action:@selector(addCredit:) forControlEvents:UIControlEventTouchUpInside];
+                [self.confirmButton addTarget:self action:@selector(sendToAddCreditAndPurchase:) forControlEvents:UIControlEventTouchUpInside];
                 [DataManager sharedManager].balanceToAdd = [totalPrice intValue] - balance;
             }
             self.userBalanceLabel.text = balanceText;
@@ -120,7 +149,7 @@
     });
 }
 
--(void)confirmPurchase:(UIButton*)sender{
+- (void)confirmPurchase:(UIButton*)sender {
     NSDictionary* showingInvoiceData = [DataManager sharedManager].showingInvoiceData;
     if (!showingInvoiceData) {
         return;
@@ -140,16 +169,51 @@
     }];
 }
 
--(void)addCredit:(UIButton*)sender{
-    [self performSegueWithIdentifier:@"ShowAddCreditSegue" sender:self];
+- (void)sendToAddCreditAndPurchase:(UIButton*)sender {
+    int balance = [DataManager sharedManager].balanceToAdd;
+    if (balance < 100) {
+        balance = 100;
+    }
+    NSString* token = [SibcheHelper getToken];
+    NSDictionary* showingInvoiceData = [DataManager sharedManager].showingInvoiceData;
+    NSDictionary* data = @{
+                           @"invoice_id": showingInvoiceData && [showingInvoiceData valueForKeyPath:@"data.id"] ? [showingInvoiceData valueForKeyPath:@"data.id"] : @"",
+                           @"price": [[NSNumber numberWithInt:balance] stringValue]
+                           };
+    [[NetworkManager sharedManager] post:@"transactions/create" withData:data withAdditionalHeaders:nil withToken:token withSuccess:^(NSString *response, NSDictionary *json) {
+        NSString* payLink = [json valueForKeyPath:@"data.attributes.pay_link"];
+        NSString* transactionId = [json valueForKeyPath:@"data.id"];
+        if (payLink && [payLink isKindOfClass:[NSString class]] && payLink.length > 0) {
+            NSString* openUrl=[NSString stringWithFormat:@"%@://SBStoreKit/transactions/%@", [DataManager sharedManager].appScheme, transactionId];
+            NSString* escapedOpenUrl = [openUrl stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+            NSString* url = [NSString stringWithFormat:@"%@?callback=%@", payLink, escapedOpenUrl];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url] options:@{} completionHandler:nil];
+            });
+        }else{
+            // TODO: We should improve user experience on errors
+            [self paymentCanceledWithNotifying:YES];
+        }
+    } withFailure:^(NSInteger errorCode, NSInteger httpStatusCode) {
+        // TODO: We should improve user experience on errors
+        [self paymentCanceledWithNotifying:YES];
+    }];
+
 }
 
+- (void)newNotification:(NSNotification*)note {
+    NSString* name = note.name;
+    if ([name isEqualToString:ADDCREDIT_SUCCESSFUL]) {
+        [self paymentSucceeded];
+    } else if ([name isEqualToString:ADDCREDIT_CANCELED]){
+        [self paymentCanceledWithNotifying:YES];
+    }
+}
 
 - (void)setLoading:(BOOL)isLoading withMessage:(NSString*)message{
     dispatch_async(dispatch_get_main_queue(), ^{
         if (isLoading || message.length > 0) {
             self.loadingView.hidden = NO;
-            self.invoiceView.hidden = YES;
             if (isLoading) {
                 [self.loadingIndicator startAnimating];
             } else {
@@ -158,7 +222,6 @@
             self.loadingLabel.text = message;
         }else{
             self.loadingView.hidden = YES;
-            self.invoiceView.hidden = NO;
         }
     });
 }
